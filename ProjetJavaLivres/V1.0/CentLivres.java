d0bd1e277622b07b0ef244d146d134b15ae9b874
Gjelal <gelalb-96@hotmail.com>
2018-03-22 14:37:28 +0100

    Ajout d'un fichier .git init et modification V1

--- ProjetJavaLivres/V1.0/CentLivres.java
+++ ProjetJavaLivres/V1.0/CentLivres.java
@@ -2,17 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-/**
- * Module 633.1-Programmation - Contrôle continu du 19.11.2016
- * 
- * Application interactive de gestion des livres
- *
- * Vous avez le droit de modifier la signature de cette classe en 
- * accord avec les différentes architectures vues en classe.
- *
- * @author Peter Daehne - HEG Genève
- * @version 1.0
- */
+
 public class CentLivres {
 
   private static final String TITRE_FRAME = "100 Livres";
